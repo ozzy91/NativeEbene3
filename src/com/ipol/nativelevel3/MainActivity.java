be37dd8a7953e3017045e401a2ed89b87a743c35
Thomas Ostfeld <thomas.ostfeld@gmail.com>
2013-11-20 16:00:31 +0100

    added second activity for top 10 testing

--- src/com/ipol/nativelevel3/MainActivity.java
+++ src/com/ipol/nativelevel3/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ipol.nativelevel3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.ipol.nativelevel3.gui.AverageBars;
+import com.ipol.nativelevel3.gui.CircleDiagram;
 import com.ipol.nativelevel3.gui.ComparisonBar;
 
 public class MainActivity extends Activity {
@@ -15,9 +18,9 @@ public class MainActivity extends Activity {
 	private ComparisonBar comparisonBar2;
 	private ComparisonBar comparisonBar3;
 	private ComparisonBar comparisonBar4;
-	private ComparisonBar comparisonBar5;
 	
 	private AverageBars averageBar1;
+	private CircleDiagram circleDiagram1;
 
 	private Button animateButton;
 	private boolean animated;
@@ -32,16 +35,16 @@ public class MainActivity extends Activity {
 		comparisonBar2 = (ComparisonBar) findViewById(R.id.comparison_bar2);
 		comparisonBar3 = (ComparisonBar) findViewById(R.id.comparison_bar3);
 		comparisonBar4 = (ComparisonBar) findViewById(R.id.comparison_bar4);
-		comparisonBar5 = (ComparisonBar) findViewById(R.id.comparison_bar5);
 		averageBar1 = (AverageBars) findViewById(R.id.average_bars1);
+		circleDiagram1 = (CircleDiagram) findViewById(R.id.circle_diagram1);
 		
 		comparisonBar1.setData("Ballkontakte", "915", "466", 66.26f);
 		comparisonBar2.setData("Ballbesitz in %", "72", "28", 72.3f);
 		comparisonBar3.setData("Passquote in %", "91", "78", 53.97f);
 		comparisonBar4.setData("Erfolgreiche Pässe", "657", "193", 77.29f);
-		comparisonBar5.setData("Kurzpässe", "629", "159", 79.82f);
 		
 		averageBar1.setData("Ballkontakte", "863", "623", 100f, 72.2f);
+		circleDiagram1.setData("Ballbesitz", 71.01f, 50f);
 	}
 	
 	public void animate(View view) {
@@ -54,9 +57,9 @@ public class MainActivity extends Activity {
 			comparisonBar2.hideBar();
 			comparisonBar3.hideBar();
 			comparisonBar4.hideBar();
-			comparisonBar5.hideBar();
 			
 			averageBar1.hideBar();
+			circleDiagram1.hideBar();
 		} else {
 			System.out.println("animate");
 			animated = true;
@@ -66,17 +69,26 @@ public class MainActivity extends Activity {
 			comparisonBar2.showBar();
 			comparisonBar3.showBar();
 			comparisonBar4.showBar();
-			comparisonBar5.showBar();
 			
 			averageBar1.showBar();
+			circleDiagram1.showBar();
 		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_settings) {
+			Intent intent = new Intent(this, Top10Activity.class);
+			startActivity(intent);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 
 }
